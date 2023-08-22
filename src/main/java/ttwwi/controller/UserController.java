fdc6b6924d29191a3831dacc4bfb56986e1cdff4
KwonJaeHoo <rainmiro@naver.com>
2023-08-22 18:55:58 +0900

    controller

--- src/main/java/ttwwi/controller/UserController.java
+++ src/main/java/ttwwi/controller/UserController.java
@@ -22,11 +22,36 @@ public class UserController
 	     * 카카오 callback
 	     * [GET] /oauth/kakao/callback
 	     */
-	    @ResponseBody
+//	    @ResponseBody
+//	    @PostMapping("/oauth2/code/kakao")
+//	    public void kakaoCallback(@RequestParam String token, Model model) 
+//	    {
+//	    	model.addAttribute("token", token);
+//	        System.out.println(token);
+//	    }
+	    
+
 	    @PostMapping("/oauth2/code/kakao")
-	    public void kakaoCallback(@RequestParam String token, Model model) 
+	    public void kakaoCallback(@RequestBody MyData accessToken) 
 	    {
-	    	model.addAttribute("accesstoken", token);
-	        System.out.println(token);
+	    	
+	        System.out.println("token : " + accessToken.getValue());
+	        //return "Value received and printed";
 	    }
+	    
+		public class MyData 
+		{
+		    private String value;
+
+		    public String getValue() 
+		    {
+		        return value;
+		    }
+
+		    public void setValue(String value) 
+		    {
+		        this.value = value;
+		    }
+		}
+
 }
