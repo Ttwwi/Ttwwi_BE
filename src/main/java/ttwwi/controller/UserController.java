15595e8ad8b303f087c60db65413e5df57fb72f6
KwonJaeHoo <rainmiro@naver.com>
2023-08-22 15:24:23 +0900

    yml

--- src/main/java/ttwwi/controller/UserController.java
+++ src/main/java/ttwwi/controller/UserController.java
@@ -1,34 +1,29 @@
-//package ttwwi.controller;
-//
-//import lombok.RequiredArgsConstructor;
-//import ttwwi.service.UserService;
-//
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/auth")
-//@RequiredArgsConstructor
-//public class UserController 
-//{
-//    private final UserService userService;
-//
-//    @PostMapping("/signup")
-//    public ResponseEntity<MemberResponseDto> signup(@RequestBody MemberRequestDto memberRequestDto) 
-//    {
-//        return ResponseEntity.ok(authService.signup(memberRequestDto));
-//    }
-//
-//    @PostMapping("/login")
-//    public ResponseEntity<TokenDto> login(@RequestBody MemberRequestDto memberRequestDto) {
-//        return ResponseEntity.ok(authService.login(memberRequestDto));
-//    }
-//
-//    @PostMapping("/reissue")
-//    public ResponseEntity<TokenDto> reissue(@RequestBody TokenRequestDto tokenRequestDto) {
-//        return ResponseEntity.ok(authService.reissue(tokenRequestDto));
-//    }
-//}
+package ttwwi.controller;
+
+import lombok.RequiredArgsConstructor;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+@RequiredArgsConstructor
+public class UserController 
+{	    /**
+	     * 카카오 callback
+	     * [GET] /oauth/kakao/callback
+	     */
+	    @ResponseBody
+	    @GetMapping("/oauth2/code/kakao")
+	    public void kakaoCallback(@RequestParam String code) 
+{
+	        System.out.println(code);
+	    }
+}
