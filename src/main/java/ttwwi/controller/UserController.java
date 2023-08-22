36cb24f07a3892782d6325255cb073fc74223cd5
KwonJaeHoo <rainmiro@naver.com>
2023-08-22 19:20:12 +0900

    accesstoken add

--- src/main/java/ttwwi/controller/UserController.java
+++ src/main/java/ttwwi/controller/UserController.java
@@ -1,7 +1,10 @@
 package ttwwi.controller;
 
 import lombok.RequiredArgsConstructor;
+import ttwwi.dto.AccessTokenDto;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,40 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/login")
 @RequiredArgsConstructor
 public class UserController 
-{	    /**
-	     * 카카오 callback
-	     * [GET] /oauth/kakao/callback
-	     */
-//	    @ResponseBody
-//	    @PostMapping("/oauth2/code/kakao")
-//	    public void kakaoCallback(@RequestParam String token, Model model) 
-//	    {
-//	    	model.addAttribute("token", token);
-//	        System.out.println(token);
-//	    }
-	    
-	@ResponseBody
-	    @PostMapping("/oauth2/code/kakao")
-	    public String kakaoCallback(@RequestBody MyData accessToken) 
-	    {
-	    	
-	        System.out.println("token : " + accessToken.getValue());
-	        return accessToken.getValue();
-	    }
-	    
-		public class MyData 
-		{
-		    private String value;
-
-		    public String getValue() 
-		    {
-		        return value;
-		    }
-
-		    public void setValue(String value) 
-		    {
-		        this.value = value;
-		    }
-		}
+{	
+	
+	@RequestMapping("/oauth2/code/kakao")
+	public ResponseEntity<String> KakaoAccessToken(@RequestBody AccessTokenDto accessToken) 
+	{
+		String info = accessToken.getAccessToken();
+		return ResponseEntity.status(HttpStatus.OK).body(info);
+	}
 
 }
