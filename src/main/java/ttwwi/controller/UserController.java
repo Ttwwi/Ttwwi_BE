7ad8ec78b887f75afd4eb7505c43749d82460c57
KwonJaeHoo <rainmiro@naver.com>
2023-08-22 19:01:40 +0900

    json

--- src/main/java/ttwwi/controller/UserController.java
+++ src/main/java/ttwwi/controller/UserController.java
@@ -2,17 +2,11 @@ package ttwwi.controller;
 
 import lombok.RequiredArgsConstructor;
 
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/login")
@@ -30,7 +24,7 @@ public class UserController
 //	        System.out.println(token);
 //	    }
 	    
-
+	@ResponseBody
 	    @GetMapping("/oauth2/code/kakao")
 	    public void kakaoCallback(@RequestBody MyData accessToken) 
 	    {
